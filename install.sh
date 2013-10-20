62edac2abb49343931efcbc49ea31785001c2269
root <root@progweb>
2013-10-20 16:06:29 +0200

    Modification du script d'installation

--- install.sh
+++ install.sh
@@ -2,8 +2,11 @@
 /usr/bin/yes | sudo apt-get update
 /usr/bin/yes | sudo apt-get install apache2 postgresql libfcgi-dev
 sudo a2enmod ssl
-rm -Rf /var/www/*
-cp -R www/ /var/
-cp -R conf_apache/* /etc/apache2/
-make -C cgi/
-cp cgi/prog.fcgi /usr/lib/cgi-bin/
+sudo rm -Rf /var/www/*
+sudo cp -R www/ /var/
+sudo cp -R conf_apache/* /etc/apache2/
+sudo make -C cgi/
+sudo cp cgi/prog.fcgi /usr/lib/cgi-bin/
+sudo service apache2 restart
+ADRESS=`ifconfig eth0 | grep "inet addr" | sed 's/.*addr:\([0-9]*\.[0-9]*\.[0-9]*\.[0-9]\).*/\1/'`
+eval echo "Site web installe a l\'adresse: \$ADRESS"
